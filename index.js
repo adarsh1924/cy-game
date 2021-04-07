a6876e110fd2522001780b5520a6a4a5fc77dd3c
Adarsh Agrawal <adarshagrawal1924@gmail.com>
2021-04-07 11:38:33 +0530

    Basic setup of game functionality

---
+++ index.js
@@ -0,0 +1,100 @@
+
+const myArray = document.querySelectorAll("td");
+
+const cross = [];
+const circle = [];
+
+let noOfTableClicks = 0;
+
+
+myArray.forEach((tableData) => {
+    tableData.addEventListener("click", function () {
+        let cellNo = this.classList[1];
+        noOfTableClicks++;
+        this.innerHTML = handleClick(noOfTableClicks, cellNo);
+        let display = document.querySelector("h2");
+        if (checkIfSomeoneWins(cross)) {
+            display.innerHTML = "cross wins";
+        } else if (checkIfSomeoneWins(circle)) {
+            display.innerHTML = "circle wins";
+        } else if (noOfTableClicks === 9) {
+            display.innerHTML = "Match Draw";
+        }
+    })
+});
+
+function handleClick(num, cellNo) {
+    if (num % 2 === 1) {
+        cross.push(cellNo);
+        return '<i class="fas fa-times fa-5x"></i>'
+    } else {
+        circle.push(cellNo);
+        return '<i class="far fa-circle fa-4x"></i>'
+    }
+}
+
+function checkIfSomeoneWins(anArray) {
+    if (horizontalMatch(anArray) || verticalMatch(anArray) || diagonalMatch(anArray)) {
+        return true;
+    }
+}
+
+function horizontalMatch(anArray) {
+
+    const newArray = anArray.map(function (element) {
+        return element.substring(1, 2)
+    });
+
+    const newArray0 = newArray.filter(function (element) {
+        return element === "0"
+    });
+
+    const newArray1 = newArray.filter(function (element) {
+        return element === "1"
+    });
+
+    const newArray2 = newArray.filter(function (element) {
+        return element === "2"
+    });
+
+    if (newArray0.length === 3 || newArray1.length === 3 || newArray2.length === 3) {
+        return true;
+    }
+
+}
+
+function verticalMatch(anArray) {
+    const newArray = anArray.map(function (element) {
+        return element.substring(2, 3)
+    });
+
+    const newArray0 = newArray.filter(function (element) {
+        return element === "0"
+    });
+
+    const newArray1 = newArray.filter(function (element) {
+        return element === "1"
+    });
+
+    const newArray2 = newArray.filter(function (element) {
+        return element === "2"
+    });
+
+    if (newArray0.length === 3 || newArray1.length === 3 || newArray2.length === 3) {
+        return true;
+    }
+}
+
+function diagonalMatch(anArray) {
+    const newArray = anArray.map(function (element) {
+        return element.substring(1, 3)
+    });
+
+    const firstDiag = newArray.includes("00") && newArray.includes("11") && newArray.includes("22")
+
+    const secondDiag = newArray.includes("02") && newArray.includes("11") && newArray.includes("20")
+
+    if (firstDiag || secondDiag) {
+        return true;
+    }
+}
